e6767fe6c0b24d2a0ed82c7094ef9987e84139f3
chetan <zambare.c123@gmail.com>
2020-12-05 09:25:09 +0530

    [Chetan] Added: Welcome message

---
+++ src/main/java/internetbooking/HotelReservationSystem.java
@@ -0,0 +1,8 @@
+package internetbooking;
+
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Reservation System");
+    }
+
+}
